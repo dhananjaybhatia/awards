5b4f55d1acb1463854100480d709e9af2e3ad5ae
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-06-09 13:23:31 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="">
+      <h1 className="font-zentry-regular text-5xl text-violet-300 font-bold">
+        Welcome to Awards
+      </h1>
+    </main>
+  );
+};
+
+export default App;
